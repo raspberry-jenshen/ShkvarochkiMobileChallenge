7e0eee02d55140ff38e3522ecc42c81bb4d234f1
Забельников Сергей <ReginFell@gmail.com>
2015-11-28 13:37:45 +0200

    Added base activity and fragment

--- app/src/main/java/com/shkvarochki/mobilechallenge/ui/BaseActivity.java
+++ app/src/main/java/com/shkvarochki/mobilechallenge/ui/BaseActivity.java
@@ -1,10 +1,12 @@
 package com.shkvarochki.mobilechallenge.ui;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 
 import com.trello.rxlifecycle.components.RxActivity;
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
-public class BaseActivity extends RxActivity {
+public class BaseActivity extends RxAppCompatActivity {
 
     public Context getContext() {
         return this;
--- app/src/main/java/com/shkvarochki/mobilechallenge/ui/screens/EditPhotoActivity.java
+++ app/src/main/java/com/shkvarochki/mobilechallenge/ui/screens/EditPhotoActivity.java
@@ -1,14 +1,16 @@
 package com.shkvarochki.mobilechallenge.ui.screens;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
 import com.shkvarochki.mobilechallenge.R;
 import com.shkvarochki.mobilechallenge.ui.BaseActivity;
 import com.shkvarochki.mobilechallenge.ui.adapters.FilterImageAdapter;
+import com.shkvarochki.mobilechallenge.utils.FilterHelper;
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Transformation;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -16,18 +18,15 @@ import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.ViewById;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-import jp.wasabeef.picasso.transformations.ColorFilterTransformation;
-import jp.wasabeef.picasso.transformations.CropSquareTransformation;
-import jp.wasabeef.picasso.transformations.gpu.ToonFilterTransformation;
 
 @EActivity(R.layout.activity_edit_photo)
 public class EditPhotoActivity extends BaseActivity {
 
     @Extra
     protected String imageUri;
+
+    @ViewById
+    protected Toolbar toolbar;
 
     @ViewById
     protected RecyclerView recyclerView;
@@ -38,17 +37,21 @@ public class EditPhotoActivity extends BaseActivity {
 
     @AfterViews
     protected void afterViews() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.edit_photo_title);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         imageUri = "/sdcard/image.jpg";
         Picasso.with(getContext()).load(new File(imageUri)).fit().centerCrop().into(imageView);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
 
-        List<Transformation> transformationList = new ArrayList<>();
-        transformationList.add(new ToonFilterTransformation(getContext()));
-        transformationList.add(new ColorFilterTransformation(android.R.color.black));
-        transformationList.add(new CropSquareTransformation());
-
-        FilterImageAdapter adapter = new FilterImageAdapter(getContext(), transformationList, imageUri);
+        FilterImageAdapter adapter = new FilterImageAdapter(getContext(), FilterHelper.getSupportedTransformationList(getContext()), imageUri);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
     }
