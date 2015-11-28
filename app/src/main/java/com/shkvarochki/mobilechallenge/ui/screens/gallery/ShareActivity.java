3af52f3f17c5a7d4f304f0a0e4a252bbeadb1257
Yazon2006 <Yazon2006@yandex.ru>
2015-11-28 15:58:48 +0200

    share toolbar and image

--- app/src/main/java/com/shkvarochki/mobilechallenge/ui/screens/gallery/ShareActivity.java
+++ app/src/main/java/com/shkvarochki/mobilechallenge/ui/screens/gallery/ShareActivity.java
@@ -2,14 +2,20 @@ package com.shkvarochki.mobilechallenge.ui.screens.gallery;
 
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import com.shkvarochki.mobilechallenge.R;
 import com.shkvarochki.mobilechallenge.ui.BaseActivity;
+import com.squareup.picasso.Picasso;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_share)
 @OptionsMenu(R.menu.menu_share)
@@ -17,6 +23,28 @@ public class ShareActivity extends BaseActivity {
 
     @Extra
     protected String imageUri;
+
+    @ViewById
+    ImageView readyPhoto_imageView;
+
+    @ViewById
+    protected Toolbar toolbar;
+
+    @AfterViews
+    protected void afterViews() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.photo_filters_title);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+        }
+
+        Picasso.with(this.getContext()).load(imageUri).fit().centerCrop().into(readyPhoto_imageView);
+    }
 
     @OptionsItem(R.id.action_share)
     protected void shareClicked() {
