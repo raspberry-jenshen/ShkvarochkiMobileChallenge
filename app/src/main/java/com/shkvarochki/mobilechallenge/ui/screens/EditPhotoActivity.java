a15990422f3999b6eb1d5e9c5e98a838b76039da
Забельников Сергей <ReginFell@gmail.com>
2015-11-28 14:16:50 +0200

    Added base activity and fragment

--- app/src/main/java/com/shkvarochki/mobilechallenge/ui/screens/EditPhotoActivity.java
+++ app/src/main/java/com/shkvarochki/mobilechallenge/ui/screens/EditPhotoActivity.java
@@ -1,20 +1,41 @@
 package com.shkvarochki.mobilechallenge.ui.screens;
 
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
 import com.shkvarochki.mobilechallenge.R;
 import com.shkvarochki.mobilechallenge.ui.BaseActivity;
+import com.shkvarochki.mobilechallenge.ui.adapters.FilterImageAdapter;
+import com.shkvarochki.mobilechallenge.utils.FilterHelper;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_edit_photo)
 public class EditPhotoActivity extends BaseActivity {
+
+    @Extra
+    protected String imageUri;
 
     @ViewById
     protected ImageView imageView;
 
     @ViewById
     protected Toolbar toolbar;
+
+    @AfterViews
+    protected void afterViews() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.photo_filters_title);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 }
--- app/src/main/java/com/shkvarochki/mobilechallenge/ui/screens/PhotoFiltersActivity.java
+++ app/src/main/java/com/shkvarochki/mobilechallenge/ui/screens/PhotoFiltersActivity.java
@@ -4,17 +4,21 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import com.shkvarochki.mobilechallenge.R;
 import com.shkvarochki.mobilechallenge.ui.BaseActivity;
 import com.shkvarochki.mobilechallenge.ui.adapters.FilterImageAdapter;
 import com.shkvarochki.mobilechallenge.utils.FilterHelper;
+import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Transformation;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.ViewById;
+
+import java.io.File;
 
 @EActivity(R.layout.activity_photo_filter)
 public class PhotoFiltersActivity extends BaseActivity implements FilterImageAdapter.OnItemClickListener {
@@ -28,6 +32,9 @@ public class PhotoFiltersActivity extends BaseActivity implements FilterImageAda
     @ViewById
     protected RecyclerView recyclerView;
 
+    @ViewById
+    protected ImageView imageView;
+
     @AfterViews
     protected void afterViews() {
         setSupportActionBar(toolbar);
@@ -40,6 +47,8 @@ public class PhotoFiltersActivity extends BaseActivity implements FilterImageAda
         }
 
         imageUri = "/sdcard/image.jpg";
+        Picasso.with(getContext()).load(new File(imageUri)).fit().centerCrop().into(imageView);
+
         GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
 
         FilterImageAdapter adapter = new FilterImageAdapter(getContext(), this, imageUri, FilterHelper.getSupportedTransformationList(getContext()));
@@ -49,5 +58,6 @@ public class PhotoFiltersActivity extends BaseActivity implements FilterImageAda
 
     @Override
     public void onClick(Transformation transformation) {
+        Picasso.with(getContext()).load(new File(imageUri)).transform(transformation).fit().centerCrop().into(imageView);
     }
 }
